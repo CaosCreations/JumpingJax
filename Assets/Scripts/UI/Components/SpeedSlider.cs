300bb67e78f4680487e1260c77d0cf0c1eb599a3
Sabien <ambid17@gmail.com>
2020-08-25 16:19:07 -0500

    reworked in game UI

---
+++ Assets/Scripts/UI/Components/SpeedSlider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedSlider : MonoBehaviour
+{
+    public RectTransform mask;
+    public Text speedText;
+    private float basePosition = -525;
+    
+    public void SetSpeed(float speed)
+    {
+        float ratio = speed / PlayerConstants.MaxVelocity;
+        float xOffset = basePosition * ratio;
+        mask.anchorMax = new Vector2(ratio, 1);
+
+
+        speedText.text = Mathf.Round(speed * 100) / 100 + "m/s";
+    }
+}
