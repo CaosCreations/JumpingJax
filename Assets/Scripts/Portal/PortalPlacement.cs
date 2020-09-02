2a5259b43987761d23edc0f7fb0c6c629358ba27
Bui360 <68082693+Bui360@users.noreply.github.com>
2020-09-01 21:23:40 -0700

    Did Crossahri thing color

--- Assets/Scripts/Portal/PortalPlacement.cs
+++ Assets/Scripts/Portal/PortalPlacement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(CameraMove))]
 public class PortalPlacement : MonoBehaviour
@@ -13,9 +14,12 @@ public class PortalPlacement : MonoBehaviour
     [SerializeField]
     private LayerMask layerMask;
 
-    // Leaving this in until crosshair is added
-    //[SerializeField]
-    //private Crosshair crosshair;
+    public Image crosshair;
+    public Sprite crosshairDefault;
+    public Sprite crosshairLeft;
+    public Sprite crosshairRight;
+    public Sprite crosshairLeftTop;
+    public Sprite crosshairRightTop;
 
     private CameraMove cameraMove;
     private PlayerPortalableController playerPortalable;
@@ -23,8 +27,36 @@ public class PortalPlacement : MonoBehaviour
     private Quaternion flippedYRotation = Quaternion.Euler(0.0f, 180.0f, 0.0f);
     private const float portalRaycastDistance = 250;
     private bool isPortalLevel = false;
+    public bool LeftPortalClick = false;
+    public bool RightPortalClick = false;
+    public int PlaceLast = 0;
+    private void CrossCheck()
+    {
+        if (LeftPortalClick == false && RightPortalClick == false)
+        {
+            crosshair.sprite = crosshairDefault;
+        }
+        else if (LeftPortalClick == true && RightPortalClick == false)
+        {
+            crosshair.sprite = crosshairLeft;
+        }
+        else if (LeftPortalClick == false && RightPortalClick == true)
+        {
+            crosshair.sprite = crosshairRight;
+        }
+        else if (LeftPortalClick == true && RightPortalClick == true)
+        {
+            if (PlaceLast == 1)
+            {
+                crosshair.sprite = crosshairLeftTop;
+            }
+            else if (PlaceLast == 2)
+            {
+                crosshair.sprite = crosshairRightTop;
+            }
+        }
 
-
+    }
     private void Awake()
     {
         cameraMove = GetComponent<CameraMove>();
@@ -42,10 +74,16 @@ public class PortalPlacement : MonoBehaviour
         if (InputManager.GetKeyDown(PlayerConstants.Portal1))
         {
             FirePortal(0, cameraMove.playerCamera.transform.position, cameraMove.playerCamera.transform.forward, portalRaycastDistance);
+            LeftPortalClick = true;
+            PlaceLast = 1;
+            CrossCheck();
         }
         else if (InputManager.GetKeyDown(PlayerConstants.Portal2))
         {
             FirePortal(1, cameraMove.playerCamera.transform.position, cameraMove.playerCamera.transform.forward, portalRaycastDistance);
+            RightPortalClick = true;
+            PlaceLast = 2;
+            CrossCheck();
         }
     }
 
