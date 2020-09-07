92efacb1af591a9d6e53c378c2753c39da073a76
Bui360 <68082693+Bui360@users.noreply.github.com>
2020-09-07 15:53:36 -0700

    Command skip

---
+++ Assets/Scripts/Misc/Console/Commands/Command Skip Checkpoint.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+[CreateAssetMenu(fileName = "New SkipCheckpoint Command", menuName = "Developer Console/Commands/SkipCheckpoint Command")]
+public class CommandSkipCheckpoint : ConsoleCommand
+{
+    public override void Process(string[] args)
+    {
+        int parsedLevel;
+        if (int.TryParse(args[0], out parsedLevel))
+        {
+            PlayerProgress player = FindObjectOfType<PlayerProgress>();
+            Checkpoint[] components = GameObject.FindObjectsOfType<Checkpoint>();
+            foreach (Checkpoint checkpoint in components)
+            {
+                if (checkpoint.level == parsedLevel)
+                {
+                    player.currentCheckpoint = checkpoint;
+                    player.Respawn();
+                }
+            }
+        }
+        else
+        {
+            Debug.LogError("Could not process checkpoint number. Please use and integer");
+        }
+    }
+}
