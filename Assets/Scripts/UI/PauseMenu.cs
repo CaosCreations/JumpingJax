f37031e94703e67922d91224a0df9d02119a8b7e
Sabien <ambid17@gmail.com>
2020-11-05 21:37:42 -0600

    fix platform dependent compilation for builds

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -102,13 +102,10 @@ public class PauseMenu : MonoBehaviour {
     }
 
     public void QuitGame() {
-        if (Application.isEditor)
-        {
-            EditorApplication.isPlaying = false;
-        }
-        else
-        {
-            Application.Quit();
-        }
+        #if UNITY_EDITOR
+                EditorApplication.isPlaying = false;
+        #else
+                    Application.Quit();
+        #endif
     }
 }
