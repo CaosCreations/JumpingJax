92c770fa80447a6034a2e092fad26c18cbf0883d
Sabien <ambid17@gmail.com>
2020-11-12 16:07:50 -0600

    added options tab buttons

--- Assets/Scripts/UI/Components/TabButton.cs
+++ Assets/Scripts/UI/Components/TabButton.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
-using static OptionsMenu;
+using UnityEngine.EventSystems;
 
-public class TabButton : MonoBehaviour
+public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public Button button;
     public Text buttonText;
     public Image image;
 
+    public Sprite activeSwoosh;
+    public Sprite inactiveSwoosh;
+
+    private Color activeColor = new Color(.58f, .93f, .76f);
+    private Color hoverColor = new Color(1, 1, 1);
+    private Color inactiveColor = new Color(0.5f, 0.5f, 0.5f);
+
+    private bool isSelected;
+
     public void Init(string buttonText, UnityAction action)
     {
+        isSelected = false;
         this.buttonText.text = buttonText;
         button.onClick.RemoveAllListeners();
         button.onClick.AddListener(action);
@@ -20,11 +30,31 @@ public class TabButton : MonoBehaviour
 
     public void SelectTab()
     {
-        image.gameObject.SetActive(true);
+        isSelected = true;
+        image.sprite = activeSwoosh;
+        buttonText.color = activeColor;
     }
 
     public void UnselectTab()
     {
-        image.gameObject.SetActive(false);
+        isSelected = false;
+        image.sprite = inactiveSwoosh;
+        buttonText.color = inactiveColor;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (!isSelected)
+        {
+            buttonText.color = hoverColor;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (!isSelected)
+        {
+            buttonText.color = inactiveColor;
+        }
     }
 }
--- Assets/Scripts/UI/LevelSelectionCard.cs
+++ Assets/Scripts/UI/LevelSelectionCard.cs
@@ -28,7 +28,6 @@ public class LevelSelectionCard : MonoBehaviour
     public Sprite noImageSprite;
 
     private Color activeColor = new Color(.58f, .93f, .76f);
-    //private Color activeColor = new Color(1, 1, 1);
     private Color inactiveColor = new Color(0.5f, 0.5f, 0.5f);
 
     public void Init(Level level)
