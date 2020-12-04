3885a254773839a2fc71e982c0f3ec1c94593629
RMA429 <robert.alaniz01@utrgv.edu>
2020-12-03 19:19:27 -0600

    Added ctrl + z undo and ctrl + shift + z redo. Also removed Menu.unity from pr

--- Assets/Scripts/LevelEditor/LevelEditorUndo.cs
+++ Assets/Scripts/LevelEditor/LevelEditorUndo.cs
@@ -8,6 +8,13 @@ public class LevelEditorUndo : MonoBehaviour
     public static Quaternion prevRotation;
     public static Vector3 prevScale;
 
+    private static bool shouldDebug = false;
+    private bool shiftPressed;
+    private bool ctrlPressed;
+    private bool zPressed;
+    private bool undo;
+    private bool redo;
+
     public static List<ICommand> commandHistory;
     static int counter;
     void Awake()
@@ -22,35 +29,82 @@ public class LevelEditorUndo : MonoBehaviour
         }
         commandHistory.Add(command);
         counter++;
-        Debug.Log("Length of Command History: " + commandHistory.Count);
+
+        if (shouldDebug == true){Debug.Log("Length of Command History: " + commandHistory.Count); }
     }
     void Update()
     {
-        //(Input.GetKey(KeyCode.RightControl) || Input.GetKey(KeyCode.LeftControl)) && Input.GetKeyDown(KeyCode.Z) if you want ctrl + z
-        if (InputManager.GetKeyDown(PlayerConstants.Undo))
+        CheckUndo();
+        if (InputManager.GetKeyDown(PlayerConstants.Undo) || undo) 
         {
-            if(counter > 0)
+            if (counter > 0)
             {
                 counter--;
                 commandHistory[counter].Undo();
+                zPressed = false;
 
-                Debug.Log($"Undo {commandHistory[counter].CommandName()}");
-                Debug.Log("Length of Command History: " + commandHistory.Count);
-                Debug.Log("Counter is at " + counter);
+                if (shouldDebug == true)
+                {
+                    Debug.Log($"Undo {commandHistory[counter].CommandName()}");
+                    Debug.Log("Length of Command History: " + commandHistory.Count);
+                    Debug.Log("Counter is at " + counter);
+                }
             }
         }
-        else if (InputManager.GetKeyDown(PlayerConstants.Redo))
+        else if (InputManager.GetKeyDown(PlayerConstants.Redo) || redo)
         {
             if (counter < commandHistory.Count)
             {
                 commandHistory[counter].Redo();
-
-                Debug.Log($"Redo {commandHistory[counter].CommandName()}"); //this debug log has to happen before the counter increments
-                
+                if (shouldDebug == true) { Debug.Log($"Redo {commandHistory[counter].CommandName()}"); } //this debug log has to happen before the counter increments
                 counter++;
-                Debug.Log("Length of Command History: " + commandHistory.Count);
-                Debug.Log("Counter is at " + counter);
+                zPressed = false;
+
+                if (shouldDebug == true)
+                {
+                    Debug.Log("Length of Command History: " + commandHistory.Count);
+                    Debug.Log("Counter is at " + counter);
+                }
             }
         }
+    }
+
+    public void CheckUndo()
+    {
+        if (Input.GetKeyDown(KeyCode.RightControl) || Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            ctrlPressed = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightControl) || Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            ctrlPressed = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            shiftPressed = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            shiftPressed = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            zPressed = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            zPressed = false;
+        }
+
+        if (ctrlPressed && zPressed && shiftPressed) { redo = true; }
+        else { redo = false; }
+
+        if (ctrlPressed && zPressed && !redo) { undo = true; }
+        else { undo = false; }
     }
 }
