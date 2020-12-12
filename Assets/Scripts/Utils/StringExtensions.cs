c39a055069abf154023e7439d80d134d549b9574
Max Hooton <61809127+maxwellpark@users.noreply.github.com>
2020-12-12 21:09:02 +0000

    String extensions

---
+++ Assets/Scripts/Utils/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class StringExtensions 
+{ 
+    public static string InsertNewLines(this string str)
+    {
+        return str.Replace("<br>", "\n");
+    }
+
+    public static string InsertCustomHotKeys(this string str)
+    {
+        foreach (KeyValuePair<string, string> kvp in HotKeyUtils.GetHotKeyMismatches())
+        {
+            str = str.Replace(kvp.Key, kvp.Value);
+        }
+        Debug.Log("New string: " + str); 
+        return str; 
+    }
+}
