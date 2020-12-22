de353de82cbacdd6e4cf5cc5bf744a0b5440a1ea
Sabien <ambid17@gmail.com>
2020-12-22 01:59:56 -0600

    clear replay file when going to next level

--- Assets/Scripts/UI/WinMenu.cs
+++ Assets/Scripts/UI/WinMenu.cs
@@ -61,6 +61,7 @@ public class WinMenu : MonoBehaviour
 
     public void NextLevel()
     {
+        GameManager.Instance.replayFileLocation = string.Empty;
         Level currentLevel = GameManager.GetCurrentLevel();
 
         if (currentLevel.workshopFilePath != string.Empty || currentLevel.levelEditorScenePath != string.Empty)
