ab6ec6da2d76279d7cd310c8ae90ad246f290ecf
Sabien <ambid17@gmail.com>
2021-01-06 16:42:31 -0600

    revert testing changes

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -45,7 +45,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (ghostCamera.enabled)// || GameManager.Instance.isLoadingScene)
+        if (ghostCamera.enabled || GameManager.Instance.isLoadingScene)
         {
             return;
         }
