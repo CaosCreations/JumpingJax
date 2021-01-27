55dcb2d43b2243f3ba2e1bdb223d62c6a029cbc3
pirolye <birolevente33@gmail.com>
2021-01-27 21:38:46 +0100

    hop2 pass2 it17

---
+++ Assets/Scripts/Player/PlayerDebug.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDebug : MonoBehaviour
+{
+    
+
+    public bool isDebugging;
+    public Vector3 normalStartPosition;
+    public Vector3 debugStartPosition;
+    private GameObject currentGameObject;
+    
+    //public bool isDebugging;
+    //public bool isDebugging;
+
+    void Start()
+    {
+        currentGameObject = this.gameObject;
+        
+        if(isDebugging == true)
+        {
+            currentGameObject.transform.position = debugStartPosition;
+            Debug.Log("Starting player mode: DEBUG");
+        }
+        else
+        {
+            currentGameObject.transform.position = normalStartPosition;
+            Debug.Log("Starting player mode: NORMAL");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
