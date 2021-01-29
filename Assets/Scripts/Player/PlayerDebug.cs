5aedd6f5f8cf5abb633d8229c1252f6359ab830e
Sabien <ambid17@gmail.com>
2021-01-29 13:02:02 -0600

    update playerdebug

--- Assets/Scripts/Player/PlayerDebug.cs
+++ Assets/Scripts/Player/PlayerDebug.cs
@@ -4,35 +4,21 @@ using UnityEngine;
 
 public class PlayerDebug : MonoBehaviour
 {
-    
-
     public bool isDebugging;
     public Vector3 normalStartPosition;
     public Vector3 debugStartPosition;
-    private GameObject currentGameObject;
     
-    //public bool isDebugging;
-    //public bool isDebugging;
-
     void Start()
     {
-        currentGameObject = this.gameObject;
-        
         if(isDebugging == true)
         {
-            currentGameObject.transform.position = debugStartPosition;
+            gameObject.transform.position = debugStartPosition;
             Debug.Log("Starting player mode: DEBUG");
         }
         else
         {
-            currentGameObject.transform.position = normalStartPosition;
+            gameObject.transform.position = normalStartPosition;
             Debug.Log("Starting player mode: NORMAL");
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
