2e40c8f0f30d273b6b7e776df7cfd21be0596bbe
Max Hooton <61809127+maxwellpark@users.noreply.github.com>
2021-01-30 19:30:12 +0000

    Utility function to get level editor objects by type

---
+++ Assets/Scripts/Utils/LevelEditorUtils.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class LevelEditorUtils : MonoBehaviour
+{
+    public static LevelEditorObject[] GetObjectsByType(ObjectType type)
+    {
+        return FindObjectsOfType<LevelEditorObject>()
+            .Where(x => x.objectType == type)
+            .ToArray();
+    }
+}
