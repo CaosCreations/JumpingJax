1b14f2b75cbe93e226512410aeeb8df9210b6b5f
Sabien <ambid17@gmail.com>
2021-02-12 18:00:45 -0600

    update resolution string, include refresh rate

--- Assets/Scripts/UI/SettingsMenu/VideoOptions.cs
+++ Assets/Scripts/UI/SettingsMenu/VideoOptions.cs
@@ -156,7 +156,7 @@ public class VideoOptions : MonoBehaviour
         List<string> options = new List<string>();
         for (int i = 0; i < resolutions.Length; i++)
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
+            string option = $"{resolutions[i].width} x {resolutions[i].height} {resolutions[i].refreshRate} hz";
             options.Add(option);
             Debug.Log(option);
         }
