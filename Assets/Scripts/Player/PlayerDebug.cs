97cbd7c0511a4651875bcc9454957c725085cc28
Levente Biro <birolevente33@gmail.com>
2021-02-27 00:44:20 +0100

    usability hotfix for PlayerDebug.cs (#321)
    * usability hotfix for player debug
    
    * Update PlayerDebug.cs
    
    Co-authored-by: scnoobi <scnoobers@gmail.com>

--- Assets/Scripts/Player/PlayerDebug.cs
+++ Assets/Scripts/Player/PlayerDebug.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class PlayerDebug : MonoBehaviour
 {
     public bool isDebugging;
-    public Vector3 normalStartPosition;
     public Vector3 debugStartPosition;
     
     void Start()
     {
+    #if UNITY_EDITOR
         if(isDebugging == true)
         {
             gameObject.transform.position = debugStartPosition;
-            Debug.Log("Starting player mode: DEBUG");
+            Debug.Log("[PlayerDebug]: Starting player mode at debug position.");
         }
         else
         {
-            gameObject.transform.position = normalStartPosition;
-            Debug.Log("Starting player mode: NORMAL");
+            Debug.Log("[PlayerDebug]: Starting player at entity editor position.");
         }
+    #endif
     }
 }
