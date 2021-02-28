c063b26e8922b1f57a8fdaf84f67cfb627449e85
danielrobnik <daniel@robnik.com.au>
2021-02-28 18:21:54 +1100

    Removed namespace from BulkPrefabCreator.cs

--- Assets/Scripts/Utils/BulkPrefabCreator.cs
+++ Assets/Scripts/Utils/BulkPrefabCreator.cs
@@ -1,51 +1,48 @@
 ﻿using UnityEditor;
 using UnityEngine;
 
-namespace DanR.JJ
+public class BulkPrefabCreator : MonoBehaviour
 {
-    public class BulkPrefabCreator : MonoBehaviour
+    private static string prefabsFolderPath = "Assets/Prefabs";
+    private static string outputFolderPath = "BulkPrefabs";
+
+    [MenuItem("Tools/CaosCreations/Create Bulk Prefabs")]
+    static void CreateBulkPrefabs()
     {
-        private static string prefabsFolderPath = "Assets/Prefabs";
-        private static string outputFolderPath = "BulkPrefabs";
+        string completeOutputPath = prefabsFolderPath + "/" + outputFolderPath;
 
-        [MenuItem("Tools/CaosCreations/Create Bulk Prefabs")]
-        static void CreateBulkPrefabs()
+        // If the output folder doesn't exist, create it.
+        if (!AssetDatabase.IsValidFolder(completeOutputPath)) AssetDatabase.CreateFolder(prefabsFolderPath, outputFolderPath);
+
+        GameObject[] selectedObjects = Selection.gameObjects;
+        foreach(GameObject currentObject in selectedObjects)
         {
-            string completeOutputPath = prefabsFolderPath + "/" + outputFolderPath;
+            // Store transforms.
+            Vector3 objectPosition = currentObject.transform.position;
+            Quaternion objectRotation = currentObject.transform.rotation;
+            Vector3 objectScale = currentObject.transform.localScale;
 
-            // If the output folder doesn't exist, create it.
-            if (!AssetDatabase.IsValidFolder(completeOutputPath)) AssetDatabase.CreateFolder(prefabsFolderPath, outputFolderPath);
+            // Reset transforms to create prefab.
+            currentObject.transform.position = Vector3.zero;
+            currentObject.transform.rotation = Quaternion.identity;
+            currentObject.transform.localScale = Vector3.one;
 
-            GameObject[] selectedObjects = Selection.gameObjects;
-            foreach(GameObject currentObject in selectedObjects)
+            // Create prefab.
+            string assetPath = AssetDatabase.GenerateUniqueAssetPath(completeOutputPath + "/" + currentObject.name + ".prefab");
+            // If it is already a prefab (as fresh imported objects are), unpack it.
+            if (PrefabUtility.IsPartOfAnyPrefab(currentObject)) PrefabUtility.UnpackPrefabInstance(currentObject, PrefabUnpackMode.Completely, InteractionMode.AutomatedAction);
+            PrefabUtility.SaveAsPrefabAssetAndConnect(currentObject, assetPath, InteractionMode.AutomatedAction, out bool operationSuccess);
+
+            // Restore transforms.
+            currentObject.transform.position = objectPosition;
+            currentObject.transform.rotation = objectRotation;
+            currentObject.transform.localScale = objectScale;
+
+            // Abort if operation failed.
+            if (!operationSuccess)
             {
-                // Store transforms.
-                Vector3 objectPosition = currentObject.transform.position;
-                Quaternion objectRotation = currentObject.transform.rotation;
-                Vector3 objectScale = currentObject.transform.localScale;
-
-                // Reset transforms to create prefab.
-                currentObject.transform.position = Vector3.zero;
-                currentObject.transform.rotation = Quaternion.identity;
-                currentObject.transform.localScale = Vector3.one;
-
-                // Create prefab.
-                string assetPath = AssetDatabase.GenerateUniqueAssetPath(completeOutputPath + "/" + currentObject.name + ".prefab");
-                // If it is already a prefab (as fresh imported objects are), unpack it.
-                if (PrefabUtility.IsPartOfAnyPrefab(currentObject)) PrefabUtility.UnpackPrefabInstance(currentObject, PrefabUnpackMode.Completely, InteractionMode.AutomatedAction);
-                PrefabUtility.SaveAsPrefabAssetAndConnect(currentObject, assetPath, InteractionMode.AutomatedAction, out bool operationSuccess);
-
-                // Restore transforms.
-                currentObject.transform.position = objectPosition;
-                currentObject.transform.rotation = objectRotation;
-                currentObject.transform.localScale = objectScale;
-
-                // Abort if operation failed.
-                if (!operationSuccess)
-                {
-                    Debug.LogError("Bulk prefab creation failed with: " + currentObject);
-                    return;
-                }
+                Debug.LogError("Bulk prefab creation failed with: " + currentObject);
+                return;
             }
         }
     }
