d562149808c345e30fdd51bad063b14665b88b82
Sabien <ambid17@gmail.com>
2021-04-08 15:40:16 -0500

    Added checkpoint start area preventing time from starting

--- Assets/Scripts/Singletons/GameManager.cs
+++ Assets/Scripts/Singletons/GameManager.cs
@@ -16,6 +16,7 @@ public class GameManager : MonoBehaviour
     public Level currentLevel;
     public float currentCompletionTime;
     public bool didWinCurrentLevel;
+    public bool hasLeftFirstCheckpoint;
     public bool shouldUseSteam;
     public bool isLoadingScene;
 
@@ -93,7 +94,7 @@ public class GameManager : MonoBehaviour
     {
         SteamClient.RunCallbacks();
 
-        if (!didWinCurrentLevel)
+        if (!didWinCurrentLevel && hasLeftFirstCheckpoint)
         {
             currentCompletionTime += Time.deltaTime;
         }
@@ -164,6 +165,7 @@ public class GameManager : MonoBehaviour
         Debug.Log($"GameManager.Init(): Scene loaded: {scene.name} with index {scene.buildIndex}");
         currentCompletionTime = 0;
         didWinCurrentLevel = false;
+        hasLeftFirstCheckpoint = false;
 
         if (scene.buildIndex == PlayerConstants.MainMenuSceneIndex || scene.buildIndex == PlayerConstants.CreditsSceneIndex)
         {
@@ -225,6 +227,12 @@ public class GameManager : MonoBehaviour
     {
         Instance.currentCompletionTime = 0;
         Instance.didWinCurrentLevel = false;
+        Instance.hasLeftFirstCheckpoint = false;
+    }
+
+    public static void LeftFirstCheckpoint()
+    {
+        Instance.hasLeftFirstCheckpoint = true;
     }
 
     public static async void FinishedLevel()
