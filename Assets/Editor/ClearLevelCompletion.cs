6a2a8364312d2ea47f03628884209af2dd485a8c
ambid17 <ambid17@gmail.com>
2025-09-06 15:49:36 -0500

    Fix last refactor error

--- Assets/Editor/ClearLevelCompletion.cs
+++ Assets/Editor/ClearLevelCompletion.cs
@@ -25,10 +25,7 @@ public class ClearLevelCompletion : EditorWindow
             {
                 level.levelSaveData.isCompleted = false;
                 level.levelSaveData.completionTime = 0;
-                level.levelSaveData.ghostRunPositions = new Vector3[0];
-                level.levelSaveData.ghostRunCameraRotations = new Vector3[0];
-                level.levelSaveData.ghostRunKeys = new KeysPressed[0];
-                level.levelSaveData.ghostRunVelocities = new float[0];
+                level.levelSaveData.recordedFrames = new RecordedFrame[0];
                 level.levelSaveData.collectiblesCollected = 0;
                 level.levelSaveData.ghostRunPlayerName = string.Empty;
                 EditorUtility.SetDirty(level);
